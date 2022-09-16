aaae2cce662e0002f3244f74a03d50946dd84dc6
Tsuyoshi Katayama <katayama933@gmail.com>
2022-09-17 06:34:42 +0900

    Add FrameCounter class

---
+++ Oocfuu/FrameCounter.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef _FRAMECOUNTER_H
+#define _FRAMECOUNTER_H
+
+#include <time.h>
+
+class FrameCounter
+{
+	clock_t m_startTime;
+	unsigned int m_counter;
+	unsigned int m_frameCount;
+
+public:
+	FrameCounter();
+
+	unsigned int getFrameCount();
+	void update();
+};
+
+extern FrameCounter g_frameCounter;
+
+#endif // !_FRAMECOUNTER_H
