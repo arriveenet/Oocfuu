e37a47c48f77a61ba7aa967de6f47252d1980a01
Tsuyoshi Katayama <katayama933@gmail.com>
2022-11-08 23:00:54 +0900

    敵キャラクタークラスを追加

---
+++ Oocfuu/enemy/Kuribo.cpp
@@ -0,0 +1,20 @@
+#include "Kuribo.h"
+
+Kuribo::Kuribo()
+{
+}
+
+Kuribo::~Kuribo()
+{
+
+}
+
+void Kuribo::update()
+{
+	
+}
+
+void Kuribo::draw()
+{
+
+}
---
+++ Oocfuu/enemy/Kuribo.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Enemy.h"
+
+class Kuribo {
+public:
+	Kuribo();
+	~Kuribo();
+
+	void update();
+	void draw();
+};
