8e8bbe68f6544829c4fbdee4af6a2bc6186fcd62
Tsuyoshi Katayama <katayama933@gmail.com>
2022-11-24 22:32:00 +0900

    AnimationControllerのメンバ変数を追加

--- Oocfuu/Sprite.cpp
+++ Oocfuu/Sprite.cpp
@@ -1,6 +1,13 @@
 #include "Sprite.h"
 
 Sprite::Sprite()
+	: m_animeCtr()
+{
+}
+
+Sprite::Sprite(glm::vec2 _size, glm::vec2 _position)
+	: m_animeCtr()
+	, Rect(_size, _position)
 {
 }
 
--- Oocfuu/Sprite.h
+++ Oocfuu/Sprite.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "Rect.h"
+#include "animation/AnimationController.h"
 
 class Sprite : public Rect{
 public:
+	AnimationController m_animeCtr;
+
 	Sprite();
+	Sprite(glm::vec2 _size, glm::vec2 _position);
 	virtual ~Sprite();
 
 	virtual void update();
