25ef212fec5c4b1012563028390307a82229596f
Tsuyoshi KATAYAMA <katayama933@gmail.com>
2023-01-13 00:26:26 +0900

    Intro画面のプレイヤーが表示されないバグを修正

--- Oocfuu/player/PlayerStateDie.cpp
+++ Oocfuu/player/PlayerStateDie.cpp
@@ -1,5 +1,6 @@
 #include "PlayerStateDie.h"
-#include "../Game.h"
+#include "Game.h"
+#include "Course.h"
 
 PlayerStateDie::PlayerStateDie()
 	: PlayerState(PLAYER_STATE_DIE)
@@ -20,6 +21,7 @@ void PlayerStateDie::update(PlayerStateContext* _pStateContext, Player* _pPlayer
 		_pPlayer->m_dead = false;
 		if (_pPlayer->m_left <= 0)
 			g_game.m_isGameOver = true;
+		g_courseManager.m_scroll = 0.0f;
 		g_game.setScreen(GAME_SCREEN_INTRO);
 	}
 }
