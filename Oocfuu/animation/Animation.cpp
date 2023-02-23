dfbe4ab2b04ed8949ad262750cb79731133002be
Tsuyoshi KATAYAMA <katayama933@gmail.com>
2023-02-23 19:11:11 +0900

    クッパのアニメーションを追加

--- Oocfuu/animation/Animation.cpp
+++ Oocfuu/animation/Animation.cpp
@@ -30,11 +30,28 @@ int Animation::initAll()
 	g_animations[ANIMATION_PLAYER_JUMP].m_rate = 15;
 	g_animations[ANIMATION_PLAYER_JUMP].m_next = ANIMATION_PLAYER_JUMP;
 
+	/* Koopa animation */
+	for (int i = TEXTURE_KOOPA_IDLE_1; i <= TEXTURE_KOOPA_IDLE_2; i++) {
+		g_animations[ANIMATION_KOOPA].m_keys.push_back(i);
+		g_animations[ANIMATION_KOOPA].m_rate = 32;
+		g_animations[ANIMATION_KOOPA].m_next = ANIMATION_KOOPA;
+	}
+
+	/* Koopa fire animation */
+	for (int i = TEXTURE_KOOPA_IDLE_FIRE_1; i <= TEXTURE_KOOPA_IDLE_FIRE_2; i++) {
+		g_animations[ANIMATION_KOOPA_FIRE].m_keys.push_back(i);
+		g_animations[ANIMATION_KOOPA_FIRE].m_rate = 32;
+		g_animations[ANIMATION_KOOPA_FIRE].m_next = ANIMATION_KOOPA;
+	}
+
+	/* Firework animation */
 	for (int i = TEXTURE_FIREWORK_1; i <= TEXTURE_FIREWORK_3; i++) {
 		g_animations[ANIMATION_FIREWORK].m_keys.push_back(i);
 		g_animations[ANIMATION_FIREWORK].m_rate = 4;
 		g_animations[ANIMATION_FIREWORK].m_next = ANIMATION_NONE;
 	}
 
+
 	return 0;
 }
+
