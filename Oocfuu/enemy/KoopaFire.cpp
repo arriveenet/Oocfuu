fb586c7ea4ba4b64d51a479fa523d02b905578ee
Tsuyoshi KATAYAMA <katayama933@gmail.com>
2023-02-25 19:44:27 +0900

    クッパファイヤークラスを追加

---
+++ Oocfuu/enemy/KoopaFire.cpp
@@ -0,0 +1,71 @@
+#include "KoopaFire.h"
+
+#include "Game.h"
+#include "sound/Sound.h"
+#include "world/Course.h"
+#include "common/common.h"
+
+
+KoopaFire::KoopaFire()
+	: m_enable(false)
+	, m_texture(TEXTURE_KOOPA_FIRE_1)
+	, m_speed(0.0f, 0.0f)
+	, m_height(0.0f)
+	, Rect(24, 8)
+{ }
+
+KoopaFire::~KoopaFire()
+{ }
+
+void KoopaFire::start(glm::vec2 _position, float _height, int _direction)
+{
+	m_enable = true;
+	m_position = _position;
+	// ファイヤーの高さをセット
+	m_height = _height;
+
+	// スピードとテクスチャの向きをセット
+	if (_direction == RECT_FLIP_NONE) {
+		m_speed.x = -KOOPA_FIRE_SPEED;
+		m_flip = RECT_FLIP_NONE;
+	}
+	else {
+		m_speed.x = KOOPA_FIRE_SPEED;
+		m_flip = RECT_FLIP_HORIZONTAL;
+	}
+
+	// ファイヤーの効果音を再生
+	g_pSound->play(SOUND_SE_ENEMY_FIRE);
+}
+
+void KoopaFire::update()
+{
+	if (!m_enable)
+		return;
+
+	if (m_position.y < m_height) {
+		m_position.y += 1.0f;
+	}
+
+	m_position += m_speed;
+
+	if (Game::m_count % 2 == 0)
+		m_texture = (m_texture == TEXTURE_KOOPA_FIRE_1) ? TEXTURE_KOOPA_FIRE_2 : TEXTURE_KOOPA_FIRE_1;
+
+	// 画面外に出たら更新を終了する
+	if ((m_position.x + m_size.x <= g_courseManager.m_scroll)
+		|| (m_position.x >= g_courseManager.m_scroll + SCREEN_WIDTH)
+	){
+		m_enable = false;
+	}
+}
+
+void KoopaFire::draw()
+{
+	if (!m_enable)
+		return;
+
+	g_textureManager.setTexture(m_texture);
+	Rect::draw();
+	g_textureManager.unbindTexture();
+}
---
+++ Oocfuu/enemy/KoopaFire.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Rect.h"
+#include "TextureManager.h"
+#include <glm/glm.hpp>
+
+#define KOOPA_FIRE_SPEED	2.0f
+
+class KoopaFire : public Rect {
+	bool m_enable;
+	TEXTURE m_texture;
+	glm::vec2 m_speed;
+	float m_height;
+
+public:
+	KoopaFire();
+	virtual ~KoopaFire();
+
+	void start(glm::vec2 _position, float _height, int _direction);
+	void update();
+	void draw();
+
+	bool isEnable()
+	{
+		return m_enable;
+	};
+};
