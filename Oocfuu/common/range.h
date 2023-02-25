b673481a9dd625e8d9893a28ca5e3563a32a0c1c
Tsuyoshi KATAYAMA <katayama933@gmail.com>
2023-02-25 19:45:26 +0900

    range.hを追加

---
+++ Oocfuu/common/range.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef _RANGE_H
+#define _RANGE_H
+
+typedef struct RANGE {
+	float start;
+	float end;
+} RANGE;
+
+
+//bool isOutOfRange(const RANGE& _range, const float& _point)
+//{
+//	if ((_range.start > _point) && (_range.end < _point))
+//		return true;
+//
+//	return false;
+//}
+
+#endif
